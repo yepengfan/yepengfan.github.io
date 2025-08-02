5b72af4b1ef2673a6989259d5fb64e99c41052c4
Ted Fan <ted.fan@flightcentre.com>
2025-08-02 19:48:52 +1000

    Add sideout menu functionality for mobile view

--- 01-HTML/04-navbar-component/js/index.js
+++ 01-HTML/04-navbar-component/js/index.js
@@ -1,5 +1,12 @@
 // Write custom JavaScript here.
 // You may ignore this file and delete if if JavaScript is not required for your challenge.
 document.getElementById("mobile-menu-button").addEventListener("click", () => {
-  document.getElementById("slideout-menu").classList.toggle("open");
+  console.log("Mobile menu button clicked");
+  const slideoutMenu = document.getElementById("slideout-menu");
+  console.log(slideoutMenu.classList.contains("open"));
+  if (slideoutMenu.classList.contains("open")) {
+    slideoutMenu.classList.remove("open");
+  } else {
+    slideoutMenu.classList.add("open");
+  }
 });
