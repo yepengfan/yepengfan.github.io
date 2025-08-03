3c18c08fdb6dfae3f32b99cb550da81f42773466
Ted Fan <ted.fan@flightcentre.com>
2025-08-03 12:48:03 +1000

    Add close button to slideout menu

--- 01-HTML/04-navbar-component/js/index.js
+++ 01-HTML/04-navbar-component/js/index.js
@@ -1,12 +1,13 @@
 // Write custom JavaScript here.
 // You may ignore this file and delete if if JavaScript is not required for your challenge.
 document.getElementById("mobile-menu-button").addEventListener("click", () => {
-  console.log("Mobile menu button clicked");
   const slideoutMenu = document.getElementById("slideout-menu");
-  console.log(slideoutMenu.classList.contains("open"));
-  if (slideoutMenu.classList.contains("open")) {
-    slideoutMenu.classList.remove("open");
-  } else {
-    slideoutMenu.classList.add("open");
-  }
+
+  slideoutMenu.classList.add("open");
+});
+
+document.getElementById("close-menu-button").addEventListener("click", () => {
+  const slideoutMenu = document.getElementById("slideout-menu");
+
+  slideoutMenu.classList.remove("open");
 });
