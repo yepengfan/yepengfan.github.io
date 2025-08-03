e4e7fd1ee583ed8a08942951d04e8574e46a731f
Ted Fan <ted.fan@flightcentre.com>
2025-08-03 13:01:27 +1000

    Update custom breakpoints

--- 01-HTML/04-navbar-component/js/index.js
+++ 01-HTML/04-navbar-component/js/index.js
@@ -2,12 +2,15 @@
 // You may ignore this file and delete if if JavaScript is not required for your challenge.
 document.getElementById("mobile-menu-button").addEventListener("click", () => {
   const slideoutMenu = document.getElementById("slideout-menu");
-
-  slideoutMenu.classList.add("open");
+  const menubar = document.getElementById("menu-bar");
+  slideoutMenu.classList.toggle("open");
+  menubar.classList.toggle("hidden");
 });
 
 document.getElementById("close-menu-button").addEventListener("click", () => {
   const slideoutMenu = document.getElementById("slideout-menu");
+  const menubar = document.getElementById("menu-bar");
 
-  slideoutMenu.classList.remove("open");
+  slideoutMenu.classList.toggle("open");
+  menubar.classList.toggle("hidden");
 });
