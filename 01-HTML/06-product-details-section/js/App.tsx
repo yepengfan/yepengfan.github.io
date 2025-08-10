e7f9571120fb8ec19a36245fc85f2ddd1ffd64eb
Ted Fan <ted.fan@flightcentre.com>
2025-08-11 09:20:30 +1000

    Update project to use ts

---
+++ 01-HTML/06-product-details-section/js/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      {/* Your React app content goes here */}
+      <h1 className="text-5xl text-center font-semibold pt-20">
+        Hello React World! This is now a React app.
+      </h1>
+    </div>
+  );
+}
+
+export default App;
--- 01-HTML/06-product-details-section/vite.config.ts
+++ 01-HTML/06-product-details-section/vite.config.ts
@@ -4,8 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: ".",
-  build: {
-    outDir: "dist",
+  esbuild: {
+    logOverride: { "this-is-undefined-in-esm": "silent" },
   },
 });
