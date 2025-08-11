4cc046cd63ca04e226eaaf90764cb0ab04f50a40
Ted Fan <ted.fan@flightcentre.com>
2025-08-11 17:43:12 +1000

    Add image section

--- 01-HTML/06-product-details-section/js/App.tsx
+++ 01-HTML/06-product-details-section/js/App.tsx
@@ -4,7 +4,7 @@ import data from "../data/data.json";
 
 function App() {
   return (
-    <div>
+    <div className="py-12 px-4">
       {/* Your React app content goes here */}
       <ProductImages images={data.images} />
       <h1 className="text-5xl text-center font-semibold pt-20">
--- 01-HTML/06-product-details-section/js/components/ProductImages.tsx
+++ 01-HTML/06-product-details-section/js/components/ProductImages.tsx
@@ -1,12 +1,35 @@
 import { ProductImagesProps } from "../types";
+import { useState } from "react";
 
 export default function ProductImages({ images }: ProductImagesProps) {
+  const [mainImage, setMainImage] = useState(images[0].image_url);
+
   return (
-    <div className="flex flex-col gap-6">
-      <img src={images[0].image_url} alt="Product" />
+    <div className="flex flex-col gap-6 ">
+      <img
+        id="main-image"
+        src={mainImage}
+        className="rounded-lg h-[400px] w-full object-cover"
+        alt="Product"
+      />
       <div className="flex gap-4 overflow-x-auto">
         {images.slice(1).map((image) => (
-          <img key={image.image_url} src={image.image_url} alt="Product" />
+          <button
+            key={image.image_url}
+            type="button"
+            className="p-0 border-none bg-transparent rounded-lg h-full w-[80px] object-cover focus:outline-none"
+            onClick={() => {
+              setMainImage(image.image_url);
+            }}
+            aria-label="Show product image"
+          >
+            <img
+              className="rounded-lg h-full w-[80px] object-cover pointer-events-none"
+              src={image.image_url}
+              alt="Product thumbnail"
+              draggable={false}
+            />
+          </button>
         ))}
       </div>
     </div>
