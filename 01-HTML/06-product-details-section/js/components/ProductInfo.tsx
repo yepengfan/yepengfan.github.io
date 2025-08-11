cc5f06111ccd27de6dccc2072a16465e746ce27e
Ted Fan <ted.fan@flightcentre.com>
2025-08-12 07:59:49 +1000

    Add available color swatches

--- 01-HTML/06-product-details-section/js/components/ProductInfo.tsx
+++ 01-HTML/06-product-details-section/js/components/ProductInfo.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ProductInfoProps } from "../types";
 
 export function Star({ color }: { color: string }) {
@@ -40,7 +41,59 @@ export function ReviewStars({ rating }: { rating: number }) {
   );
 }
 
+export function ColorSwatch({
+  color,
+  isSelected,
+  onClick,
+}: {
+  color: string;
+  isSelected: boolean;
+  onClick: () => void;
+}) {
+  return (
+    <button
+      type="button"
+      className={`flex items-center justify-center inline-block p-[9.33px] ${
+        isSelected
+          ? "w-[38px] h-[38px] border border-solid border-indigo-700"
+          : "w-[38px] h-[38px]"
+      }  rounded-full bg-${color === "green" ? "emerald-500" : "yellow-600"}`}
+      aria-pressed={isSelected}
+      onClick={onClick}
+      tabIndex={0}
+      style={{ cursor: "pointer" }}
+      onKeyDown={(e) => {
+        if (e.key === "Enter" || e.key === " ") {
+          e.preventDefault();
+          onClick();
+        }
+      }}
+    >
+      {isSelected ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="28"
+          height="28"
+          viewBox="0 0 28 28"
+          fill="none"
+        >
+          <path
+            d="M11.6674 17.6993L22.3918 6.97485L24.0417 8.62477L11.6674 20.9991L4.24272 13.5745L5.89263 11.9246L11.6674 17.6993Z"
+            fill="white"
+          />
+        </svg>
+      ) : null}
+    </button>
+  );
+}
+
 export default function ProductInfo(props: ProductInfoProps) {
+  const [selectedColor, setSelectedColor] = useState(props.colors[0]);
+
+  function handleColorClick(color: string) {
+    setSelectedColor(color);
+  }
+
   return (
     <div className="flex flex-col gap-8">
       <div className="flex flex-col gap-5">
@@ -80,12 +133,12 @@ export default function ProductInfo(props: ProductInfoProps) {
         </h3>
         <div className="flex gap-4">
           {props.colors.map((color, index) => (
-            <span
+            <ColorSwatch
               key={index}
-              className={`inline-block p-[9px] w-[38px] h-[38px] rounded-full bg-${
-                color === "green" ? "emerald-500" : "yellow-600"
-              }`}
-            ></span>
+              color={color}
+              isSelected={selectedColor === color}
+              onClick={() => handleColorClick(color)}
+            />
           ))}
         </div>
       </div>
