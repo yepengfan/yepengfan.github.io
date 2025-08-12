998c50543b7b567505ae6b71e3c11a574307bd70
Ted Fan <ted.fan@flightcentre.com>
2025-08-12 15:53:32 +1000

    Add available sizes section

--- 01-HTML/06-product-details-section/js/components/ProductInfo.tsx
+++ 01-HTML/06-product-details-section/js/components/ProductInfo.tsx
@@ -1,127 +1,14 @@
 import { useState } from "react";
 import { ProductInfoProps } from "../types";
-
-export function ReviewStars({ rating }: { rating: number }) {
-  const starBackground = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="18"
-      height="18"
-      viewBox="0 0 18 18"
-      fill="none"
-    >
-      <path
-        d="M8.53834 0.609964C8.70914 0.199318 9.29086 0.199318 9.46166 0.609964L11.5278 5.57744C11.5998 5.75056 11.7626 5.86885 11.9495 5.88383L17.3123 6.31376C17.7556 6.3493 17.9354 6.90256 17.5976 7.19189L13.5117 10.6919C13.3693 10.8139 13.3071 11.0053 13.3506 11.1876L14.5989 16.4208C14.7021 16.8534 14.2315 17.1954 13.8519 16.9635L9.26063 14.1592C9.10062 14.0615 8.89938 14.0615 8.73937 14.1592L4.14806 16.9635C3.76851 17.1954 3.29788 16.8534 3.40108 16.4208L4.64939 11.1876C4.69289 11.0053 4.6307 10.8139 4.48831 10.6919L0.402413 7.19189C0.0646446 6.90256 0.244408 6.3493 0.687735 6.31376L6.05054 5.88383C6.23744 5.86885 6.40024 5.75056 6.47225 5.57744L8.53834 0.609964Z"
-        fill="#E5E7EB"
-      />
-    </svg>
-  );
-
-  const fullStar = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="18"
-      height="18"
-      viewBox="0 0 18 18"
-      fill="none"
-    >
-      <path
-        d="M8.53834 0.609964C8.70914 0.199318 9.29086 0.199318 9.46166 0.609964L11.5278 5.57744C11.5998 5.75056 11.7626 5.86885 11.9495 5.88383L17.3123 6.31376C17.7556 6.3493 17.9354 6.90256 17.5976 7.19189L13.5117 10.6919C13.3693 10.8139 13.3071 11.0053 13.3506 11.1876L14.5989 16.4208C14.7021 16.8534 14.2315 17.1954 13.8519 16.9635L9.26063 14.1592C9.10062 14.0615 8.89938 14.0615 8.73937 14.1592L4.14806 16.9635C3.76851 17.1954 3.29788 16.8534 3.40108 16.4208L4.64939 11.1876C4.69289 11.0053 4.6307 10.8139 4.48831 10.6919L0.402413 7.19189C0.0646446 6.90256 0.244408 6.3493 0.687735 6.31376L6.05054 5.88383C6.23744 5.86885 6.40024 5.75056 6.47225 5.57744L8.53834 0.609964Z"
-        fill="#FACC15"
-      />
-    </svg>
-  );
-
-  const halfStar = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="18"
-      height="18"
-      viewBox="0 0 18 18"
-      fill="none"
-    >
-      <defs>
-        <linearGradient id="halfFill" x1="0%" y1="0%" x2="100%" y2="0%">
-          <stop offset="50%" stopColor="#FACC15" />
-          <stop offset="50%" stopColor="#E5E7EB" />
-        </linearGradient>
-      </defs>
-      <path
-        d="M8.53834 0.609964C8.70914 0.199318 9.29086 0.199318 9.46166 0.609964L11.5278 5.57744C11.5998 5.75056 11.7626 5.86885 11.9495 5.88383L17.3123 6.31376C17.7556 6.3493 17.9354 6.90256 17.5976 7.19189L13.5117 10.6919C13.3693 10.8139 13.3071 11.0053 13.3506 11.1876L14.5989 16.4208C14.7021 16.8534 14.2315 17.1954 13.8519 16.9635L9.26063 14.1592C9.10062 14.0615 8.89938 14.0615 8.73937 14.1592L4.14806 16.9635C3.76851 17.1954 3.29788 16.8534 3.40108 16.4208L4.64939 11.1876C4.69289 11.0053 4.6307 10.8139 4.48831 10.6919L0.402413 7.19189C0.0646446 6.90256 0.244408 6.3493 0.687735 6.31376L6.05054 5.88383C6.23744 5.86885 6.40024 5.75056 6.47225 5.57744L8.53834 0.609964Z"
-        fill="url(#halfFill)"
-      />
-    </svg>
-  );
-
-  const TOTAL_STARS = 5;
-
-  return (
-    <div className="flex flex-row gap-1">
-      {Array(TOTAL_STARS)
-        .fill(starBackground)
-        .map((_, i) => {
-          let starIcon;
-          if (i < Math.floor(rating)) {
-            starIcon = fullStar;
-          } else if (i < Math.ceil(rating)) {
-            starIcon = halfStar;
-          } else {
-            starIcon = starBackground;
-          }
-          return <span key={i}>{starIcon}</span>;
-        })}
-    </div>
-  );
-}
-
-export function ColorSwatch({
-  color,
-  isSelected,
-  onClick,
-}: {
-  color: string;
-  isSelected: boolean;
-  onClick: () => void;
-}) {
-  return (
-    <button
-      type="button"
-      className={`flex items-center justify-center inline-block p-[9.33px] ${
-        isSelected
-          ? "w-[38px] h-[38px] border border-solid border-indigo-700"
-          : "w-[38px] h-[38px]"
-      }  rounded-full bg-${color === "green" ? "emerald-500" : "yellow-600"}`}
-      aria-pressed={isSelected}
-      onClick={onClick}
-      tabIndex={0}
-      style={{ cursor: "pointer" }}
-      onKeyDown={(e) => {
-        if (e.key === "Enter" || e.key === " ") {
-          e.preventDefault();
-          onClick();
-        }
-      }}
-    >
-      {isSelected ? (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="28"
-          height="28"
-          viewBox="0 0 28 28"
-          fill="none"
-        >
-          <path
-            d="M11.6674 17.6993L22.3918 6.97485L24.0417 8.62477L11.6674 20.9991L4.24272 13.5745L5.89263 11.9246L11.6674 17.6993Z"
-            fill="white"
-          />
-        </svg>
-      ) : null}
-    </button>
-  );
-}
+import ReviewStars from "./ReviewStars";
+import ColorSwatch from "./ColorSwatch";
 
 export default function ProductInfo(props: ProductInfoProps) {
   const [selectedColor, setSelectedColor] = useState(props.colors[0]);
+  const [quantity, setQuantity] = useState(1);
+
+  const { colors } = props || [];
+  const sizes = [...new Set(props.inventory.map((item) => item.size))];
 
   function handleColorClick(color: string) {
     setSelectedColor(color);
@@ -160,19 +47,80 @@ export default function ProductInfo(props: ProductInfoProps) {
         </div>
       </div>
       <p className="text-base font-normal">{props.description}</p>
-      <div className="flex flex-col gap-4">
-        <h3 className="text-neutral-500 text-base font-normal">
-          Available Colors
-        </h3>
-        <div className="flex gap-4">
-          {props.colors.map((color, index) => (
-            <ColorSwatch
-              key={index}
-              color={color}
-              isSelected={selectedColor === color}
-              onClick={() => handleColorClick(color)}
-            />
-          ))}
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-4">
+          <h3 className="text-neutral-500 text-base font-normal">
+            Available Colors
+          </h3>
+          <div className="flex gap-4">
+            {colors.map((color, index) => (
+              <ColorSwatch
+                key={index}
+                color={color}
+                isSelected={selectedColor === color}
+                onClick={() => handleColorClick(color)}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h3 className="text-neutral-500 text-base font-normal">
+            Available Sizes
+          </h3>
+          <div className="flex gap-4 overflow-x-auto">
+            {sizes.map((size) => (
+              <div
+                className="border border-neutral-200 rounded py-[12px] px-[20px] uppercase"
+                key={size}
+              >
+                <h4>{size}</h4>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h3 className="text-neutral-500 text-base font-normal">Quantity</h3>
+          <div className="w-fit bg-neutral-50 flex items-center gap-3 border border-neutral-200 rounded px-4 py-2">
+            <button
+              onClick={() =>
+                setQuantity((quantity) =>
+                  quantity - 1 >= 0 ? quantity - 1 : 0
+                )
+              }
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="17"
+                viewBox="0 0 18 17"
+                fill="none"
+                className="p-[1.67px]"
+              >
+                <path
+                  d="M13.861 7.97209H4.13879V9.36098H13.861V7.97209Z"
+                  fill="#A3A3A3"
+                />
+              </svg>
+            </button>
+            <p className="w-[49px] h-[32px] px-3 py-1.5 flex items-center justify-center text-sm font-medium">
+              {quantity}
+            </p>{" "}
+            <button onClick={() => setQuantity((quantity) => quantity + 1)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="17"
+                viewBox="0 0 18 17"
+                fill="none"
+                className="p-[1.67px]"
+              >
+                <path
+                  d="M8.30546 7.9721V3.80544H9.69435V7.9721H13.861V9.36099H9.69435V13.5277H8.30546V9.36099H4.13879V7.9721H8.30546Z"
+                  fill="#525252"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
       <ul>
