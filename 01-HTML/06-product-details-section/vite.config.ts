2a9702d419ebeb5ba7669b6b7f486dc31dbae83c
Ted Fan <ted.fan@flightcentre.com>
2025-08-20 18:01:37 +1000

    Update config

--- 01-HTML/06-product-details-section/vite.config.ts
+++ 01-HTML/06-product-details-section/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/01-HTML/06-product-details-section/",
   esbuild: {
     logOverride: { "this-is-undefined-in-esm": "silent" },
   },
