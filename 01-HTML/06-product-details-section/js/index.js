14f713bfd45cf0c0dfda76a7b237408a309de777
Ted Fan <ted.fan@flightcentre.com>
2025-08-24 14:23:19 +1000

    Update code structure

--- 01-HTML/06-product-details-section/js/index.js
+++
@@ -1,2 +0,0 @@
-// Write custom JavaScript here.
-// You may ignore this file and delete if if JavaScript is not required for your challenge.
--- 01-HTML/06-product-details-section/vite.config.ts
+++ 01-HTML/06-product-details-section/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "yepengfan.github.io/01-HTML/06-product-details-section/",
+  base: ".",
   build: {
     outDir: "dist",
     assetsDir: "assets",
